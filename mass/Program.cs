49b56afceb83c6bf9e8801a412b4b6e9d1b4d28e
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2023-11-09 15:53:58 +0200

    Add files via upload

---
+++ mass/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace mass
+{
+    internal class Program
+    {
+        static int[] InPut()
+        {
+            Write("Введите размерность массива: ");
+            byte n = Convert.ToByte(ReadLine());
+            int[] array = new int[n];
+
+            Random random = new Random();
+
+            for (byte i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-5, 5);
+            }
+            return array;
+        }
+
+        static void Output(int[] array)
+        {
+            foreach(var element in array)
+            {
+                Write($"{element};\t");
+            }
+        }
+
+        static int Count(int[] array)
+        {
+            int count = 0;
+            foreach (var element in array)
+            {
+                if (element > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static int Max(int[] array)
+        {
+            int max = 0;
+            foreach(var element in array)
+            {
+                if (element > max)
+                {
+                    max = element;
+                }
+            }
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] array = InPut();
+                Output(array);
+                WriteLine($"\n\nКол-во положительных элементов:{Count(array)}");
+                WriteLine($"\nМаксимальный элемент массива: {Max(array)}");
+
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Исключение: {ex.Message}");
+                WriteLine($"Метод: {ex.TargetSite}");
+                WriteLine($"Трассировка стека: {ex.StackTrace}");
+            }
+            ReadKey();
+        }
+    }
+}
